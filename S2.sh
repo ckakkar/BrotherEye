b0d784876c866ad77436894c1e856bc9e1bac968
Cyrus Kakkar <cyruskakkar@Cyruss-MacBook-Pro.local>
2023-07-29 04:17:00 -0700

    Failsafe online

---
+++ S2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+echo "The pressence"
+
+sleep 2
+
+echo "Lucife"
+
+sleep 2
+
+echo "Ellaine"
+
+sleep 2
+
+echo "Michael"
+
+sleep 2
+
+echo "CAS"
+
+sleep 2
+
+echo "Mandrakk"
+
+sleep 2
+
+echo "Mxy"
+
+sleep 2
+
+echo "Manhattan"
+
+sleep 2
+
+echo "AKA Failsafe ONLINE"
+
